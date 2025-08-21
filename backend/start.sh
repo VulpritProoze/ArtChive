7adbfa2a282c08c3bfcd8fe14ed9d4f41b2f507f
Maika <guintocaleb.com>
2025-08-21 17:54:57 +0800

    nigana najd tawn

--- backend/start.sh
+++ backend/start.sh
@@ -7,7 +7,7 @@ python manage.py migrate
 python manage.py loaddata common/fixtures/trophy_types.json
 python manage.py loaddata common/fixtures/award_types.json
 
-# Try to start server
+#Try to start server
 python manage.py runserver 0.0.0.0:8000 || {
     echo "Django server exited — keeping container alive for debugging"
     tail -f /dev/null
