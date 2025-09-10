d1a628b54a54a6a60fb224bfa70de42d8c9ece59
Vulprit Prooze <raileyalin@gmail.com>
2025-09-10 22:31:01 +0800

    added prompt when unauthorized user attempts to enter collective (frontend)

--- frontend/src/components/auth/collective-protected-route.tsx
+++ frontend/src/components/auth/collective-protected-route.tsx
@@ -1,4 +1,6 @@
 import { Outlet, Navigate, useParams } from 'react-router-dom'
+import { useEffect } from 'react'
+import { toast } from 'react-toastify'
 import { useAuth } from '@context/auth-context'
 
 export default function CollectiveProtectedRoute() {
@@ -9,15 +11,14 @@ export default function CollectiveProtectedRoute() {
     if (isMember) {
         return <Outlet />
     }
-    
-    if (!collectiveId) {
-        console.error('Collective Id is missing to check for user\'s membership status')
-        
-    }
 
-    if (!isMember) {
-        console.error('User is not a member of this collective.')
-    }
+    useEffect(() => {
+        if (!collectiveId) {
+          toast.error("Collective ID is missing in URL");
+        } else if (!isMember) {
+          toast.info("You need to join this collective to access this page");
+        }
+    }, [collectiveId, isMember]);
 
     return <Navigate to='/collective' replace />
 }
