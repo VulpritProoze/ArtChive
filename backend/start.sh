304637603aaf6cb1ce7c0c5d1273c32ba363f6e3
Vulprit Prooze <raileyalin@gmail.com>
2025-11-06 15:17:21 +0800

    fix

--- backend/start.sh
+++ backend/start.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -e
 
-python manage.py makemigrations # remove in prod
-python manage.py migrate        # remove in prod
+# python manage.py makemigrations # remove in prod
+# python manage.py migrate        # remove in prod
 
-python manage.py loaddata common/fixtures/trophy_types.json
-python manage.py loaddata common/fixtures/award_types.json
-python manage.py loaddata common/fixtures/default_collectives.json
-python manage.py create_initial_data
+# python manage.py loaddata common/fixtures/trophy_types.json
+# python manage.py loaddata common/fixtures/award_types.json
+# python manage.py loaddata common/fixtures/default_collectives.json
+# python manage.py create_initial_data
 
 #Try to start server
 python manage.py runserver 0.0.0.0:8000 || {
